ea4e737e1cb007170c911ee4cd355fb29dc271f4
Kenneth Ulloa <ulloakenth@gmail.com>
2024-06-16 19:27:10 -0500

    docs: add comment to init function of themes

--- src/theme/infrastructure/__init__.py
+++ src/theme/infrastructure/__init__.py
@@ -1,6 +1,9 @@
 def init():
+    """
+    Initializes the theme module by registering the appropriate implementations
+    based on the current environment.
+    """
     from dependify import register
     from .mongo_repository import MongoDBThemeRepository, IThemeRepository
 
     register(IThemeRepository, MongoDBThemeRepository)
-
