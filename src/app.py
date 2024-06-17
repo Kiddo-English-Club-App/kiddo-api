b245f2e2b9794db025420b1cd84320bf9f970d6f
Kenneth Ulloa <ulloakenth@gmail.com>
2024-06-16 19:31:50 -0500

    docs: add comments to app creation and main entrypoint

--- src/app.py
+++ src/app.py
@@ -1,9 +1,20 @@
+"""
+This module is the entry point of the application. It creates the Flask app and loads the controllers.
+"""
+
 from flask import Flask
 
 
 def load_controllers(app: Flask):
+    """
+    Loads the controllers into the Flask app. This function should be called at the beginning of the application
+    to ensure that all controllers are registered.
+
+    :param app: The Flask app instance
+    """
     from flask import Blueprint
 
+    # Import controllers for registration
     api = Blueprint("api", __name__, url_prefix="/api")
     from account.controller import controller as account_controller
     from player.controller import controller as player_controller
@@ -11,21 +22,32 @@ def load_controllers(app: Flask):
     from services.token_controller import controller as token_controller
     from theme.controller import controller as theme_controller
 
+    # Register controllers with the API blueprint
     api.register_blueprint(account_controller)
     api.register_blueprint(theme_controller)
     api.register_blueprint(player_controller)
     api.register_blueprint(token_controller)
+
+    # Register the API blueprint with the app
     app.register_blueprint(api)
+
+    # Register the services controller
     app.register_blueprint(services_controller)
 
 
 def create_app() -> Flask:
+    """
+    Creates the Flask application instance.
+
+    :return: The Flask application instance
+    """
     app = Flask(__name__)
     import settings
     import settings.dependencies as dependencies
     import settings.environment as environment
     import settings.logs as logs
 
+    # Initialize the application settings (environment, dependencies, error handlers, logs)
     settings.init(app)
 
     load_controllers(app)
@@ -36,6 +58,9 @@ def create_app() -> Flask:
     def health():
         return {"status": "OK", "environment": environment.env.ENV}, 200
 
+    # Add test endpoints if the environment is testing
+    # This is useful for testing the application in a CI/CD pipeline
+    # The test endpoints allow the database to be initialized and populated with mock data
     if environment.env.is_testing():
         logs.logger().info("Testing environment detected. Adding test endpoints.")
 
--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
+"""
+This is the main entry point for the Flask application.
+
+It creates the Flask app and runs it on the default port.
+"""
 from app import create_app
 
-app = create_app()
 
 if __name__ == "__main__":
+    # Create the Flask app and run it on the default port
+    # The app is created using the create_app function from the app module
+    app = create_app()
     app.run()
